bcc0079802085dac8d806e59946d45a6a46b7ad7
dwandrew <danwandrew@gmail.com>
2020-08-25 09:18:37 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+let tasks = document.getElementById("tasks")
+
+
+let submitForm = document.getElementById('create-task-form')
+
+submitForm.addEventListener("click", function(event) {
+  let content = document.getElementById("new-task-description")
+  let text = content.value
+    if (text !== ""){
+      let tasks = document.getElementById("tasks")
+      let listItem = document.createElement("li")
+      let listText = document.createTextNode(`${text}`)
+      // let button = document.createElement("button")
+      // button.innerText = `Delete`
+      // button.className = `delete-button`
+      listItem.appendChild(listText)
+      // listItem.appendChild(button)
+      tasks.appendChild(listItem)
+      content.value = ""
+    }
+
+
+  event.preventDefault();
+  
+}, false);
+
+// let deleteButtons = document.getElementsByClassName("delete-button")
+
+// deleteButtons.addEventListener("click", function(event) {
+//   event.parentNode.parentNode.deleteChild();
+// });
+
+
+
+
+
+  
 });

2defacba8d64720b61b681efe873dcd1d99a506a
shazy89 <erdo.shazy123@gmail.com>
2020-08-26 18:44:48 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const submit = document.getElementById("submit")
+  submit.addEventListener('click', (e) => {
+    addTodo();
+    e.preventDefault();
+  })
+  let remove = document.getElementById('remove');
 });
+
+  
+function addTodo() {
+  const newTask = document.getElementById("new-task-description").value;
+  const allTasks = document.getElementById("tasks")
+   let task = document.createElement("li")
+    task.id = "t-id"
+    task.textContent = newTask;
+    allTasks.appendChild(task);
+    document.getElementById("new-task-description").value = null
+};
+
+
+

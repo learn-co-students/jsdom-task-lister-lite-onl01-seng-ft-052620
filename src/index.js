a1cb28db93457dc3bcf20cdbb0446c429947a82e
Luca <lucakanyo@protonmail.com>
2020-08-26 16:25:34 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  form = document.querySelector("#create-task-form")
+  form.addEventListener("submit", function(event){
+    event.preventDefault();
+    addItem();
+  }, false)
+
 });
+
+addItem = function(){
+  p = document.createElement('p')
+  p.innerText = document.querySelector("#new-task-description").value
+  document.querySelector('#tasks').appendChild(p)
+}
+
+//it's okay for a start I guess
+// will come back for the extra tasks later
+// love the pink!

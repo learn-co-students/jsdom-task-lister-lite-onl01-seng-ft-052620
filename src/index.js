84854cede10d820f3d25a5179a71daca662321f2
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-08-27 12:43:46 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+const form = document.getElementById("create-task-form");
+form.addEventListener("submit", (e) => {
+  addTodo();
+  e.preventDefault(); 
+})
 });
+function addTodo() {
+  let task = document.getElementById("new-task-description").value 
+  if (task !== "") {
+    let taskList = document.getElementById("tasks");
+    let li = document.createElement("li");
+    li.textContent = task 
+    taskList.appendChild(li)
+  }
+};
+
+
+//what should happen
+
+//whats the cause
+
+//when should we be able to do this

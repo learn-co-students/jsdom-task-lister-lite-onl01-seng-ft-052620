1e4331248df27c073218de26e54d5bdaccffa069
KaelinAlexander <kaelin.alexander@gmail.com>
2020-08-28 14:25:13 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const submit = document.getElementsByTagName('input')[1]
+  const output = document.getElementsByTagName('ul')[0]
+
+  submit.addEventListener('click', function(event) {
+    event.preventDefault();
+    let text = document.getElementById('new-task-description').value
+    let li = document.createElement('li')
+    li.innerHTML = text
+    debugger;
+    alert ('I was clicked!');
+    output.appendChild(li);
+   });
+ 
 });

ae5b5145de91fcb788f7f64384c458aa12d20310
barrettanderson <barrettanderson1989@gmail.com>
2020-08-28 21:03:32 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form')
+  const newTask = document.getElementById('new-task-description')
+  const todoList = document.getElementById('tasks')
+
+  form.addEventListener("submit", function(event){
+    const li = document.createElement('li');
+    li.innerHTML = newTask.value;
+    todoList.appendChild(li);
+    
+    const button = document.createElement("button");
+    button.innerHTML = "x";
+    li.appendChild(button);
+    event.preventDefault();
+
+    function removeElement(li) {
+      li.parentNode.removeChild(li)
+    }
+    
+    button.addEventListener("click", removeElement(li));
+    
+
+  });
 });

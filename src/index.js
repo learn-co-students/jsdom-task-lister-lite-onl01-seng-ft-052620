554113c73c3dc01e96c9da98ecdfda5eda198067
github username <github email address>
2020-08-29 20:08:14 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
+const form = () => document.querySelector('form');
+const input = () => document.querySelector('input#new-task-description');
+const taskList = () => document.querySelector('ul#tasks')
+
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("create-task-form");
-  form.addEventListener("submit", (e) => {
-    addTodo();
-    e.preventDefault(); 
-  })
+  // be able to submit
+  form().addEventListener('submit', createTodo)
+
 });
 
-function addTodo() {
-  let task = document.getElementById("new-task-description").value 
-  if (task !== "") {
-    let taskList = document.getElementById("tasks");
-    let li = document.createElement("li");
-    li.textContent = task 
-    taskList.appendChild(li)
-  }
-};
+function createTodo(e) {
+  e.preventDefault();
+
+  const title = input().value
+
+  const li = document.createElement('li');
+  li.innerText = title;
+
+  const button = document.createElement('button');
+  button.innerText = "X"
+
+  button.addEventListener('click', function() {
+    this.parentNode.remove()
+  })
+
+  li.appendChild(button);
+  taskList().appendChild(li);
+
+
+  resetInput();
+}
+
+function resetInput() {
+  input().value = ""
+}

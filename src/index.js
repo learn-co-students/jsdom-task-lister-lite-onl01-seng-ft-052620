bb0d89c6deef8a840b34db8cc26794d479781ae5
Jenae Christensen <729jenae.christensen@gmail.com>
2020-09-03 13:08:15 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,73 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById('create-task-form');
-  const newTask = document.getElementById('new-task-description');
-  const todoList = document.getElementById('tasks');
+const form = () => document.querySelector('form');
+const input= () => document.querySelector('input#new-task-description');
+const select = () => document.querySelector('select');
+const taskList = () => document.querySelector('ul#tasks')
 
-  form.addEventListener("submit", function(event) {
-    const li = document.createElement('li');
-    li.innerHTML = newTask.value;
-    todoList.appendChild(li);
-    event.preventDefault();
-  });
+const highPriority = []
+const mediumPriority = []
+const lowPriority = []
+
+document.addEventListener("DOMContentLoaded", () => {
+  // be able to submit
+  form().addEventListener('submit', createTodo)
 });
+
+function createTodo(e) {
+  e.preventDefault();
+  
+  const title = input().value 
+  const priority = select().value 
+  
+  const li = document.createElement('li');
+  li.innerText = title;
+  
+  const button = document.createElement('button');
+  button.innerText = "X"
+  
+  button.addEventListener('cick', deleteTask)
+  
+  li.appendChild(button);
+  
+  if (priority === "high") {
+    li.classList.add('red')
+    highPriority.push(li);
+  } else if (priority ==="medium") {
+    li.classList.add('yellow')
+    mediumPriority.push(li)
+  } else {
+    li.classList.add('green')
+    lowPriority.push(li)
+  }
+  
+  displayTasks();
+  
+  resetInput();
+}
+
+function deleteTask() {
+  this.parentNode.remove()
+}
+
+function clearTaskLists() {
+  clearTaskLists();
+  
+  displayPriority(highPriority)
+  displayPriority(mediumPriority)
+  displayPriority(lowPriority)
+}
+
+function displayPriority(array) {
+  array.forEach(li => taskList().appendChild(li))
+}
+
+function displayPriority(array) {
+  array.forEach(li => taskList().appendChild(li))
+}
+
+function resetInput() {
+  input().value = ""
+}
+
+
+  
+  

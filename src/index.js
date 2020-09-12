f75aa743d68085240f99075b5a81c310de5c7035
ejguzman3988 <eriberto.guzman@outlook.com>
2020-09-12 00:11:29 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("create-task-form").addEventListener("submit", (e) => {
+    let list = document.createElement("li")
+    list.innerHTML = document.getElementById("new-task-description").value;
+    document.getElementById("tasks").appendChild(list);
+    e.preventDefault();
+    e.target.reset();
+  }, false)
 });
